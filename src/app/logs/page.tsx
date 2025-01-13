98bc5bc8b8c91237b6ce14e988e007db76086ae4
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-13 16:03:10 +0900

    feat: 기본 페이지 라우팅

---
+++ src/app/logs/page.tsx
@@ -0,0 +1,11 @@
+import Logs from '@/components/logs';
+
+export default function Beers() {
+  return (
+    <div>
+      <main>
+        <Logs />
+      </main>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import BeerList from '@/components/beerList';
-import Logs from '@/components/logs';
-
 export default function Home() {
   return (
     <div>
-      <main>
-        <Logs />
-        <BeerList />
-      </main>
-      <footer></footer>
+      <main>Intro page</main>
     </div>
   );
 }
