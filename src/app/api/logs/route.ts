b12a7c8e5aea4942bb14d7c19418ea41c1a9278e
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-17 16:24:32 +0900

    fix: 빌드 에러 해결

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -4,11 +4,6 @@ import { NextResponse } from 'next/server';
 const notion = new Client({ auth: process.env.SECRET_NOTION_KEY });
 const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGE_ID ?? '';
 
-const API_BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'https://staging-beer-next.vercel.app'
-    : 'http://localhost:3000';
-
 /**
  * @returns Route Handler 생성
  */
@@ -18,18 +13,18 @@ export async function GET() {
       database_id: databaseId,
     });
 
+    // 여기서 `revalidate` 옵션을 적용하지 않고, 클라이언트에서 사용
     const response = NextResponse.json(data);
 
+    // 서버에서 `revalidate` 옵션을 적용하려면 캐시 관련 설정을 사용
     response.headers.set(
       'Cache-Control',
       'public, s-maxage=600, stale-while-revalidate=300'
     );
 
-    response.headers.set('Access-Control-Allow-Origin', API_BASE_URL);
-    response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-
-    return NextResponse.json(data);
+    return response;
   } catch (error) {
+    console.error('Error fetching logs from Notion', error);
     return NextResponse.json(
       { error: 'Failed to fetch logs' },
       { status: 500 }
--- src/components/logs.tsx
+++ src/components/logs.tsx
@@ -1,16 +1,31 @@
-import { getLogData } from '@/data/log';
 import { mapNotionApi } from '@/lib/mapNotionApi';
 import LogBox from './logBox';
+import { log } from 'console';
+
+const API_BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'https://staging-beer-next.vercel.app'
+    : 'http://localhost:3000';
 
 export default async function Logs() {
-  const logs = await getLogData();
+  const res = await fetch(`${API_BASE_URL}/api/logs`, {
+    next: { revalidate: 600 },
+  });
+
+  if (!res.ok) {
+    return <div>Failed to load logs</div>;
+  }
+
+  const logs = await res.json();
+
+  if (!log) {
+    return <div>로그 데이터 패치에 실패하였습니다.</div>;
+  }
 
   return (
     <div>
-      {logs ? (
-        mapNotionApi(JSON.parse(logs)).map(log => (
-          <LogBox key={log.order} {...log} />
-        ))
+      {mapNotionApi(logs).length > 0 ? (
+        mapNotionApi(logs).map(log => <LogBox key={log.id} {...log} />) // 실제 데이터를 사용
       ) : (
         <div>로그 데이터가 존재하지 않습니다.</div>
       )}
