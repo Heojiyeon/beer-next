641dda1207936f5ec24467feb65871f51922bc6d
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-21 16:56:19 +0900

    style: Intro 페이지 반응형 구현

--- src/app/(contents)/page.tsx
+++ src/app/(contents)/page.tsx
@@ -2,7 +2,7 @@ import Information from '@/components/information';
 
 export default function Home() {
   return (
-    <main className="w-[70%] content-center">
+    <main className="w-[70%] content-center min-h-[60vh] md:min-h-full">
       <Information />
     </main>
   );
