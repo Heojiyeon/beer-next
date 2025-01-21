9b09b6d821b4cd60f6e05eaa949fe7b189ec5dc5
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-21 09:54:24 +0900

    refactor: route handler -> 일반 호출 함수 변경

--- src/app/(contents)/logs/page.tsx
+++ src/app/(contents)/logs/page.tsx
@@ -1,20 +1,32 @@
 import LogList from '@/components/logList';
+import callNotionApi from '@/lib/callNotionApi';
 
 export const revalidate = 600;
 
 export default async function Logs() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/logs`, {
-    next: { revalidate },
-  });
+  try {
+    const data = await callNotionApi();
 
-  console.log('res in logs page: ', res);
+    return (
+      <main className="w-[70%]">
+        <p className="text-pageHead text-card-description mb-6">
+          ISR 방식을 활용해 데이터 패칭을 진행했어요
+        </p>
+        <LogList res={data} />
+      </main>
+    );
+  } catch (error) {
+    console.error('Failed to fetch data in ISR:', error);
 
-  return (
-    <main className="w-[70%]">
-      <p className="text-pageHead text-card-description mb-6">
-        ISR 방식을 활용해 데이터 패칭을 진행했어요
-      </p>
-      <LogList res={res} />
-    </main>
-  );
+    return (
+      <main className="w-[70%]">
+        <p className="text-pageHead text-card-description mb-6">
+          ISR 방식을 활용해 데이터 패칭을 진행했어요
+        </p>
+        <p className="text-pageHead text-red-500 mb-6">
+          데이터를 불러오지 못했습니다.
+        </p>
+      </main>
+    );
+  }
 }
