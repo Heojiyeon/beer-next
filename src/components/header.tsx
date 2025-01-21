12daeb55947c7e50ffa2c452ff2acca077fdbfb4
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-21 10:48:29 +0900

    style: 헤더 padding 값 적용

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="py-[3rem] w-full flex justify-between">
+    <div className="p-[3rem] w-full flex justify-between">
       <div className="text-[30px]">Beer Next</div>
       <ul className="flex w-[30%] justify-around items-center">
         <li className="text-[20px]">
