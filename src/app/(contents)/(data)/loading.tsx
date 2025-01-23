6525c62a05fd9fa1bfe8b0ca152f70c49134665b
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-23 15:04:03 +0900

    style: 반응형 수정

--- src/app/(contents)/(data)/loading.tsx
+++ src/app/(contents)/(data)/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex-1 min-h-[80vh] flex flex-col justify-center items-center">
       <Skeleton className="h-4 min-w-[300px]" />
       <p className="mt-4 text-pageSubHead text-card-description">Loading..</p>
     </div>
