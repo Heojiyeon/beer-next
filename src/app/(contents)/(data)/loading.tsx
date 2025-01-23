82ddefe05d59e2bbb79f0cb500638981d9fec0a2
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-23 14:52:34 +0900

    feat: suspense(loading) 적용

---
+++ src/app/(contents)/(data)/loading.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Skeleton className="h-4 min-w-[300px]" />
+      <p className="mt-4 text-pageSubHead text-card-description">Loading..</p>
+    </div>
+  );
+}
