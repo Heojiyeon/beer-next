d6d2191fa1c3e21b1c7cde162f4321f7a3048c92
Zena <33304871+Heojiyeon@users.noreply.github.com>
2025-01-25 11:11:45 +0900

    fix: 오타 수정

--- src/components/information/index.tsx
+++ src/components/information/index.tsx
@@ -9,7 +9,7 @@ export default function Information() {
         <p>
           Next.js를 학습해보고 싶었어요. (근데 이제 15의 신기술을 찍먹해보는...)
         </p>
-        <p>Reat 19의 다양한 변경점을 적용해보고 싶었어요.</p>
+        <p>React 19의 다양한 변경점을 적용해보고 싶었어요.</p>
         <p>Vercel, Github Action을 활용한 배포 자동화를 경험해보고 싶었어요.</p>
         <p>PWA를 경험하고, 또 Storybook을 스스로 구축해 사용해보고 싶었어요.</p>
       </div>
